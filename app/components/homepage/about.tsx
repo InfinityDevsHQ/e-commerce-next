20c563c60faeeec1a3ea3417c8f256ce398845a4
Danish Asif <danishasifdev@gmail.com>
2024-08-15 19:31:23 +0500

    fix: fixed about section

--- app/components/homepage/about.tsx
+++ app/components/homepage/about.tsx
@@ -28,15 +28,18 @@ export default function InfoSection() {
   ];
 
   return (
-    <div className="bg-primary py-12 px-4 sm:px-6 lg:px-8">
+    <div className="bg-primaryHalfWhite p-4 py-12 sm:p-8 md:p-12">
       <div className="max-w-7xl mx-auto">
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 justify-items-center sm:justify-items-start gap-8">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 justify-items-center lg:justify-items-start gap-8 md:gap-12">
           {features.map((feature, index) => (
-            <div key={index} className="flex items-center">
-              <div className="flex-shrink-0 mr-4 text-2xl">{feature.icon}</div>
+            <div
+              key={index}
+              className="flex items-center justify-center gap-2.5 md:max-w-[227px]"
+            >
+              <div className="flex-shrink-0 text-2xl">{feature.icon}</div>
               <div>
-                <h3 className="text-lg font-semibold">{feature.title}</h3>
-                <p className="text-sm text-gray-600">{feature.description}</p>
+                <h3 className="font-bold text-dark">{feature.title}</h3>
+                <p className="text-sm text-darkLight">{feature.description}</p>
               </div>
             </div>
           ))}
